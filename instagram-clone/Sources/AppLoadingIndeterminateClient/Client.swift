5b5e6cba11e50e3ae2463f75e8e2285cfce0e77b
AtmanChen <samadhihuang@gmail.com>
2025-04-29 16:53:11 +0800

    add appLoading shared key

--- instagram-clone/Sources/AppLoadingIndeterminateClient/Client.swift
+++ instagram-clone/Sources/AppLoadingIndeterminateClient/Client.swift
@@ -1,8 +1,15 @@
 import Foundation
 import DependenciesMacros
+import Sharing
 
 @DependencyClient
 public struct AppLoadingIndeterminateClient: Sendable {
 	public var isLoading: @Sendable () async -> AsyncStream<Bool> = { .never }
 	public var updateLoading: @Sendable (_ showLoading: Bool) async -> Void
+}
+
+extension SharedKey {
+	public static func appLoading() -> Self where Self == InMemoryKey<Bool>.Default {
+		Self[.inMemory("appLoading"), default: false]
+	}
 }
